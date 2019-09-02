ad70a566afa524cc2f2c600462d0d10c481ad8fb
AwesomePresent <yoursmile128@hotmail.com>
2019-09-02 12:08:20 -0700

    modified procfile to say npm start

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
   console.log('production app started');
-
-
+  app.use(express.static("client/build"));
 }
 
 // // React Deployment script
