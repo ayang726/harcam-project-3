c33e17c3ea2cdf2e20484e9bae0ba6d6b542d743
AwesomePresent <yoursmile128@hotmail.com>
2019-09-02 10:10:56 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// React Deployment script
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // Define API routes here
 app.use(routes);
 
