851c86eef6526a5c8232197513d691baa1cb1975
AwesomePresent <yoursmile128@hotmail.com>
2019-09-02 11:15:03 -0700

    ready for deployement

---
+++ client/build/precache-manifest.ce98a48ef9fba905751e7913697c68a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a01eaa2a3a0793eb71e",
+    "url": "/static/js/main.76ed20b4.chunk.js"
+  },
+  {
+    "revision": "5f83a38e5caa119cbe09",
+    "url": "/static/js/2.63feb907.chunk.js"
+  },
+  {
+    "revision": "4a01eaa2a3a0793eb71e",
+    "url": "/static/css/main.6e2a143f.chunk.css"
+  },
+  {
+    "revision": "7cbcba2ac652e05202dd4a06fa4906ab",
+    "url": "/index.html"
+  }
+];
