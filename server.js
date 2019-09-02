b4e6bdf5350717df5f0f932cd5c6afa8cfd6eb32
AwesomePresent <yoursmile128@hotmail.com>
2019-09-02 12:03:36 -0700

    added procfile

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  console.log("I'm called");
+  app.use(express.static("client/build"));
+  console.log('production app started');
+
 
 }
 
